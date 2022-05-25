ca70ca5d4c2dda82e89d64bfcc81578fe3060626
helenaperdigueiro <helena.perdigueiro@gmail.com>
2022-05-25 12:27:21 -0300

    aula05 yml

---
+++ account-service/src/main/java/com/dh/accountservice/controller/AccountServiceController.java
@@ -0,0 +1,27 @@
+package com.dh.accountservice.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class AccountServiceController {
+    @Value("${message}")
+    private String message;
+    @Value("${global-message}")
+    private String globalMessage;
+
+    @RequestMapping(method = RequestMethod.GET, path = "service")
+    public Map<String, String> message() {
+        Map<String, String> response = new HashMap<>();
+
+        response.put("message", message);
+        response.put("global-message", globalMessage);
+
+        return response;
+    }
+}
