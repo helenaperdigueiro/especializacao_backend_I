8c9743a26aa046bd0782011fab8921d468fd9f92
helenaperdigueiro <helena.perdigueiro@gmail.com>
2022-05-30 18:19:49 -0300

    config-cloud yml

--- account-service/src/main/java/com/dh/accountservice/controller/AccountServiceController.java
+++ account-service/src/main/java/com/dh/accountservice/controller/AccountServiceController.java
@@ -1,6 +1,7 @@
 package com.dh.accountservice.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,19 +10,26 @@ import java.util.HashMap;
 import java.util.Map;
 
 @RestController
+@RequestMapping("/testando")
 public class AccountServiceController {
-    @Value("${message}")
-    private String message;
-//    @Value("${global-message}")
-//    private String globalMessage;
+//    @Value("${message}")
+//    private String message;
+////    @Value("${global-message}")
+////    private String globalMessage;
+//
+//    @RequestMapping(method = RequestMethod.GET, path = "service")
+//    public Map<String, String> message() {
+//        Map<String, String> response = new HashMap<>();
+//
+//        response.put("message", message);
+////        response.put("global-message", globalMessage);
+//
+//        return response;
+//    }
 
-    @RequestMapping(method = RequestMethod.GET, path = "service")
-    public Map<String, String> message() {
-        Map<String, String> response = new HashMap<>();
-
-        response.put("message", message);
-//        response.put("global-message", globalMessage);
-
-        return response;
+    @GetMapping()
+    public String testar() {
+        return "Deu certo!!";
     }
+
 }
